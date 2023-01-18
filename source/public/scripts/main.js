afd26c950d07314887624edfe277b83a900a1c06
Rogerio Oliveira <rogeriodeob@hotmail.com>
2023-01-18 12:49:55 +0000

    Initial commit with pullback mechanic

--- source/public/scripts/main.js
+++ source/public/scripts/main.js
@@ -1,53 +1,101 @@
-"use strict"
+class Bait {
+    x = oracle.width/2
+    y = oracle.height
+    radius = 20
+    target = {
+        x: this.x,
+        dx: 0,
+        y: this.y,
+        dy: 0
+    }
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+    draw = (context) => {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'red';
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = '#003300';
+        context.stroke();
+    }
+    throw = (angle, velocity) => {
+        const range = (Math.pow(velocity, 2) * 1) / 10 // Assuming 45° inclination
 
-// Global object
-var panorama = {}
-
-// Return a vector with the color and its dark correspondent
-panorama.getColors = function () {
-	"use strict"	// Allow exceptions related to good practices to be thrown
-	
-	var red = Math.ceil(194 * Math.random()) + 64, green = Math.ceil(194 * Math.random()) + 64, blue = Math.ceil(194 * Math.random()) + 64,
-	    color = 'rgb(' + red + ',' + green + ',' + blue + ')',
-        darkColor = 'rgb(' + Math.ceil(red / 1.5) + ',' + Math.ceil(green / 1.5) + ',' + Math.ceil(blue / 1.5) + ')'
-
-	return {color: color, darkColor: darkColor}
+        this.target = {
+            x: this.x + (Math.cos(angle) * range),
+            dx: Math.cos(angle) * velocity,
+            y: this.y + (Math.sin(angle) * range),
+            dy: Math.sin(angle) * velocity,
+        }
+    }
+    update = (delta) => {
+        console.log(this.x, this.y, this.target)
+        if (Math.pow(this.target.x - this.x, 2) <= Math.pow(this.target.dx, 2)) {
+            console.log("reached")
+            return
+        } 
+        this.x += this.target.dx * delta
+        this.y += this.target.dy * delta
+    }
 }
 
+const setUpPanorama = () => {
+    const oracle = document.getElementById('oracle')
+    oracle.width = window.innerWidth
+    oracle.height = window.innerHeight
+    oracle.context = oracle.getContext('2d') 
+    oracle.context.scale(oracle.width/1000, oracle.height/1000)
+
+    window.onresize = () => {
+        oracle.width = window.innerWidth
+        oracle.height = window.innerHeight
+        oracle.context.scale(oracle.width/1000, oracle.height/1000)
+    }
+
+    return {
+        oracle,
+    }
+}
+
+const setUpPullBackListeners = (document, handler) => {
+    let p1 = {}
+    let p2 = {}
+    const getPullBackValues = () => {
+        return {
+            angle: Math.atan2(p2.y - p1.y, p2.x - p1.x),
+            velocity: Math.sqrt( Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) ) / 5
+        }
+    }
+    document.addEventListener('mousedown', ({screenX, screenY}) => {
+        p2 = {x: screenX, y: screenY}
+    })
+
+    document.addEventListener('mouseup', ({screenX, screenY}) => {
+        p1 = {x: screenX, y: screenY}
+        handler(getPullBackValues())
+    })
+}
 
 function beginPanorama() {
+    const {oracle} = setUpPanorama()
+    const {context} = oracle
 
-    function instantiatePanorama() {
-        panorama.buttons = {}
-        panorama.buttons.b1 = document.getElementById('b1')
-        panorama.buttons.b2 = document.getElementById('b2')
-        panorama.buttons.b3 = document.getElementById('b3')
-        panorama.buttons.b4 = document.getElementById('b4')
-        panorama.buttons.b5 = document.getElementById('b5')
-        
-        panorama.entities = []
+    const bait = new Bait(oracle.width/2, oracle.height)
 
-        panorama.oracle = document.getElementById('oracle')
-        panorama.oracle.width = window.innerWidth
-        panorama.oracle.height = window.innerHeight
-        panorama.oracle.context = panorama.oracle.getContext('2d') 
-        panorama.oracle.context.scale(panorama.oracle.width/1000, panorama.oracle.height/1000)
+    const pullBackHandler = ({angle, velocity}) => {
+        bait.throw(angle, velocity)
+    }
+    setUpPullBackListeners(oracle, pullBackHandler)
 
-        window.onresize = () => {
-            panorama.oracle.width = window.innerWidth
-            panorama.oracle.height = window.innerHeight
-            panorama.oracle.context.scale(panorama.oracle.width/1000, panorama.oracle.height/1000)
-        }
-    } 
 
-    instantiatePanorama()
-
-	panorama.loopFunction = function runPanorama() {
-        panorama.oracle.context.clearRect(0, 0, 1000, 1000)
-        panorama.oracle.context.fillStyle = `red`
-        panorama.oracle.context.rect(1,1,113,500)
-        panorama.oracle.context.fill()
-
+    const loopDelta = 0.033
+	loopFunction = function runPanorama() {
+        context.clearRect(0, 0, 1000, 1000)
+        bait.update(loopDelta)
+        bait.draw(context)
 	}
-	panorama.loopControl = setInterval(panorama.loopFunction, 33)
+	const loopControl = setInterval(loopFunction, loopDelta * 1000)
 }
